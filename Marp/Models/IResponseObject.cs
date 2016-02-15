50d7598bdc934802a9d797a830b4d8f05043e03a
China Yu <china_yu@launchlabs.ph>
2016-02-15 11:18:41 +0800

    Add files for working with REST API

---
+++ Marp/Models/IResponseObject.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading;
+
+namespace RestService
+{
+	public interface IResponseObject
+	{
+		void Serialize(string content);	
+		void HandleException(Exception e, CancellationTokenSource source);
+	}
+}
+
