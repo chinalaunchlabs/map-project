68942c4e9dad38b418ee585fb6a10c98cb47f1c4
China Yu <china_yu@launchlabs.ph>
2016-02-16 14:08:27 +0800

    Add Map behavior to make pins a bindable property

--- Marp/ExtendedMap.cs
+++ Marp/ExtendedMap.cs
@@ -1,20 +1,52 @@
 ﻿using System;
 using Xamarin.Forms.Maps;
 using System.Collections.Generic;
+using Xamarin.Forms;
+using Marp.Models;
 
 namespace Marp
 {
-	public class ExtendedMap: Map
+	public class ExtendedMapBehavior: Behavior<Map>
 	{
-		private IList<Pin> _staticPins;
-		public IList<Pin> StaticPins {
-			get { return _staticPins; }
-			set {
-				_staticPins = value;
-				foreach (var pin in value) {
-					this.Pins.Add (pin);
-				}
+		private Map _map;
+
+		public static readonly BindableProperty ItemsSourceProperty =
+			BindableProperty.Create<ExtendedMapBehavior, IEnumerable<MyLocation>> (
+				p => p.ItemsSource, null, BindingMode.TwoWay, null, ItemsSourceChanged
+			);
+
+		public IEnumerable<MyLocation> ItemsSource {
+			get { return (IEnumerable<MyLocation>)GetValue (ItemsSourceProperty); }
+			set { SetValue (ItemsSourceProperty, value); }
+		}
+
+		private static void ItemsSourceChanged(BindableObject bindable, IEnumerable<MyLocation> oldVal, IEnumerable<MyLocation> newVal) {
+			var behavior = bindable as ExtendedMapBehavior;
+			if (behavior == null)
+				return;
+			behavior.AddPins ();
+		}
+
+		private void AddPins() {
+			System.Diagnostics.Debug.WriteLine ("ExtendedMapBehavior::AddPins()");
+			_map.Pins.Clear ();
+			if (ItemsSource == null)
+				return;
+			foreach (var item in ItemsSource) {
+				_map.Pins.Add (new Pin() {
+					Label = item.Address,
+					Position = item.Position
+				});
 			}
+		}
+
+		protected override void OnAttachedTo (Map bindable)
+		{
+			base.OnAttachedTo (bindable);
+			bindable.BindingContextChanged += (sender, e) => {
+				BindingContext = _map.BindingContext;
+			};
+			_map = bindable;
 		}
 	}
 }
--- Marp/Views/MapPage.xaml.cs
+++ Marp/Views/MapPage.xaml.cs
@@ -15,39 +15,23 @@ namespace Marp
 		public MapPage ()
 		{
 			InitializeComponent ();
-			map = new Map () {
-				IsShowingUser = true,
-				HeightRequest = 100,
-				WidthRequest = 960,
-				VerticalOptions = LayoutOptions.FillAndExpand
-			};
-
-			var stack = new StackLayout { 
-				Spacing = 0,
-				Children = {
-					map
-				}
-			};
-			Content = stack;
-			Padding = new Thickness (0, Device.OnPlatform (20, 0, 0), 0, 0);
-		}
-
-//		public void MoveTo(Position p) {
-//			System.Diagnostics.Debug.WriteLine ("MapPage::Moving to {0}, {1}", p.Latitude, p.Longitude);
-//			map.MoveToRegion (new MapSpan (p, 0.1, 0.1));
-//		}
+			map = _map;
+//			map = new Map () {
+//				IsShowingUser = true,
+//				HeightRequest = 100,
+//				WidthRequest = 960,
+//				VerticalOptions = LayoutOptions.FillAndExpand
+//			};
 //
-//		public void AddPins() {
-//			foreach (var location in App.LocationsInSession) {
-//				var latlng = location.geometry.location;
-//				map.Pins.Add (
-//					new Pin() {
-//						Position = new Position(latlng.lat, latlng.lng),
-//						Label = location.formatted_address
-//					}
-//				);
-//			}
-//		}
+//			var stack = new StackLayout { 
+//				Spacing = 0,
+//				Children = {
+//					map
+//				}
+//			};
+//			Content = stack;
+//			Padding = new Thickness (0, Device.OnPlatform (20, 0, 0), 0, 0);
+		}
 	}
 }
 
